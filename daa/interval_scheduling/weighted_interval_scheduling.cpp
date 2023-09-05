015b4552c6b44697f56e546132e893e7774143e2
Jaideep Reddy <pjaideepr@gmail.com>
2023-09-05 11:55:51 +0530

    weighted interval scheduling

--- daa/interval_scheduling/weighted_interval_scheduling.cpp
+++ daa/interval_scheduling/weighted_interval_scheduling.cpp
@@ -1,28 +1,29 @@
 // dp solution
-// -------------------------------- find sol
+// reccursively call for max weight of current_request included and when not included
 
 #include<bits/stdc++.h>
 using namespace std;
 
-map<int,int> dp = {{0,0}};
-vector<vector<int>> max_weight_requests;
+vector<vector<int>> requests;
+map<int,int> dp;
 
 int by_second(vector<int> &a, vector<int> &b) {
     return a[1] < b[1];
 }
 
-int max_weight(vector<vector<int>> requests, int s) {
-    if(dp[s] != 0) return dp[s];
-    int next_request = s;
-    while(requests[next_request][0] < requests[s][1]) 
-    return max(requests[s][2]+max_weight(requests, ))
+int max_weight(int i) {
+    if(i >= requests.size()) return 0;
+    if(dp[i]) return dp[i];
+    int i_next = i+1;
+    while((i_next < requests.size()) && (requests[i][1] > requests[i_next][0])) i_next++; 
+    dp[i] = max((requests[i][2] + max_weight(i_next)), max_weight(i+1));
+    return dp[i];
 }
 
 int main(){
     int t;
     cin >> t;
     int start, finish, weight;
-    vector<vector<int>> requests;
     while(t--) {
         cin >> start;
         cin >> finish;
@@ -30,7 +31,7 @@ int main(){
         requests.push_back({start, finish, weight});
     }
 
-    sort(requests.begin(), requests.end(), by_second);
+    sort(requests.begin(), requests.end());
 
-    cout << max_weight(requests, requests[0][0]);
+    cout << max_weight(0);
 }
